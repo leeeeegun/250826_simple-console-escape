d8c72609f831dac0ab731ad5fe75b9660b8891bc
leeeeegun <leeeeegun@naver.com>
2025-08-26 11:52:40 +0900

    feat: [이의건] 게임 메시지 및 UI 디자인 기본 구성 [SCE-BACK-05]

--- src/main/java/com/codingrecipe/simpleconsoleescape/setup/MapSetup.java
+++ src/main/java/com/codingrecipe/simpleconsoleescape/setup/MapSetup.java
@@ -15,7 +15,7 @@ public class MapSetup {
     public void createMap() {
 
         // 1. 맵/방 생성
-        Room startRoom = new Room("소환사의 협곡에 오신 걸 환영합니다.");
+        Room startRoom = new Room("이 곳은 많은 소환사가 거쳐간 협곡입니다.");
         Room corridor = new Room("바위 게가 있는 골목입니다. 북쪽과 남쪽으로 길이 나 있습니다.");
         Room baronRoom = new Room("바론이 지키고 있는 방입니다. 바론을 잡으면 버프가 생깁니다.");
         Room portalRoom = new Room("협곡으로 귀환할 수 있는 곳입니다. 포털이 막혀 있습니다.");
---
+++ src/main/java/com/codingrecipe/simpleconsoleescape/view/ConsoleView.java
@@ -0,0 +1,53 @@
+package com.codingrecipe.simpleconsoleescape.view;
+
+import com.codingrecipe.simpleconsoleescape.model.Item;
+import com.codingrecipe.simpleconsoleescape.model.Monster;
+import com.codingrecipe.simpleconsoleescape.model.Player;
+import com.codingrecipe.simpleconsoleescape.model.Room;
+
+public class ConsoleView {
+
+    public void showWelcomeMessage() {
+        System.out.println("소환사의 협곡에 오신 걸 환영합니다!");
+    }
+
+    public void showRoomInfo(Room currentRoom) {
+        System.out.println("\n------------------------------");
+        System.out.println("현재 위치: " + currentRoom.getDescription());
+        if (currentRoom.getMonster() != null) {
+            System.out.println("으악!! " + currentRoom.getMonster().getName() + "이(가) 나타났습니다!");
+        }
+        if (currentRoom.getItem() != null) {
+            System.out.println(currentRoom.getItem().getName() + "이(가) 바닥에 떨어져 있습니다.");
+        }
+        System.out.print("출구: ");
+        for (String direction : currentRoom.getExit().getExits().keySet()) {
+            System.out.print(direction + " ");
+        }
+        System.out.println("\n------------------------------");
+    }
+
+    public void showInventory(Player player) {
+        System.out.println("--- 인벤토리 ---");
+        if(player.getInventory().isEmpty()) {
+            System.out.println("비어있음");
+        } else {
+            for (Item item : player.getInventory()) {
+                System.out.println(item.toString());
+            }
+        }
+        System.out.println("----------------");
+    }
+
+    public void showBattleStatus(Player player, Monster monster) {
+        System.out.println("Player HP: " + player.getHp() + " | Monster HP: " + monster.getHp());
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void showPrompt() {
+        System.out.print("> ");
+    }
+}
