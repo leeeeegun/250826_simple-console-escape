476d29a06b9199ef79003524a4b8bdc29e3932a9
leeeeegun <leeeeegun@naver.com>
2025-08-26 10:14:47 +0900

    Initial commit

---
+++ src/main/java/com/codingrecipe/simpleconsoleescape/SimpleConsoleEscapeApplication.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.simpleconsoleescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConsoleEscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleConsoleEscapeApplication.class, args);
+    }
+
+}
