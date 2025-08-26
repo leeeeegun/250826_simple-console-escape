fb59661032b15da8ffc92c419218cab6fb9a1ea1
leeeeegun <leeeeegun@naver.com>
2025-08-26 11:35:55 +0900

    update: [이의건] 구조 변경 - 게임 맵 및 요소 클래스 [SCE-BACK-04]

--- src/main/java/com/codingrecipe/simpleconsoleescape/dungeon/Game.java
+++ src/main/java/com/codingrecipe/simpleconsoleescape/dungeon/Game.java
@@ -1,37 +1,39 @@
-package com.codingrecipe.simpleconsoleescape.dungeon;
-
-import com.codingrecipe.simpleconsoleescape.model.Item;
-import com.codingrecipe.simpleconsoleescape.model.Monster;
-import com.codingrecipe.simpleconsoleescape.model.Player;
-import com.codingrecipe.simpleconsoleescape.model.Room;
-
-public class Game {
-    private Player player;
-    private Room currentRoom;
-
-    public Game() {
-        // 1. 게임 요소 생성
-        player = new Player(100, 15);
-
-        // 2. 맵/방 생성
-        Room startRoom = new Room("소환사의 협곡에 오신 걸 환영합니다.");
-        Room corridor = new Room("바위 게가 있는 골목입니다. 북쪽과 남쪽으로 길이 나 있습니다.");
-        Room baronRoom = new Room("바론이 지키고 있는 방입니다. 바론을 잡으면 버프가 생깁니다.");
-        Room portalRoom = new Room("협곡으로 귀환할 수 있는 곳입니다. 포털이 막혀 있습니다.");
-        Room exitRoom = new Room("거대환 포털이 있는 방입니다. 귀환할 수 있는 출구인 것 같습니다.");
-
-        // 3. 방의 출구 설정 (지도 그리기)
-        startRoom.setExits(corridor, null, null, null);
-        corridor.setExits(baronRoom, null, startRoom, null);
-        baronRoom.setExits(null, portalRoom, corridor, null);
-        portalRoom.setExits(null, null, null, baronRoom);
-        portalRoom.setExits(exitRoom, null, null, baronRoom);
-        
-        // 4. 아이템 및 몬스터 배치
-        baronRoom.setMonster(new Monster("바론", 100, 20));
-        baronRoom.setItem(new Item("바론 버프", "초고속 귀환"));
-
-        // 5. 시작 위치 설정
-        currentRoom = startRoom;
-    }
-}
+//package com.codingrecipe.simpleconsoleescape.dungeon;
+//
+//import com.codingrecipe.simpleconsoleescape.model.Item;
+//import com.codingrecipe.simpleconsoleescape.model.Monster;
+//import com.codingrecipe.simpleconsoleescape.model.Player;
+//import com.codingrecipe.simpleconsoleescape.model.Room;
+//
+//import java.util.Scanner;
+//
+//public class Game {
+//    private Player player;
+//    private Room currentRoom;
+//
+//    public Game() {
+//        // 1. 게임 요소 생성
+//        player = new Player(100, 15);
+//
+//        // 2. 맵/방 생성
+//        Room startRoom = new Room("소환사의 협곡에 오신 걸 환영합니다.");
+//        Room corridor = new Room("바위 게가 있는 골목입니다. 북쪽과 남쪽으로 길이 나 있습니다.");
+//        Room baronRoom = new Room("바론이 지키고 있는 방입니다. 바론을 잡으면 버프가 생깁니다.");
+//        Room portalRoom = new Room("협곡으로 귀환할 수 있는 곳입니다. 포털이 막혀 있습니다.");
+//        Room exitRoom = new Room("거대환 포털이 있는 방입니다. 귀환할 수 있는 출구인 것 같습니다.");
+//
+//        // 3. 방의 출구 설정 (지도 그리기)
+//        startRoom.setExits(corridor, null, null, null);
+//        corridor.setExits(baronRoom, null, startRoom, null);
+//        baronRoom.setExits(null, portalRoom, corridor, null);
+//        portalRoom.setExits(null, null, null, baronRoom);
+//        portalRoom.setExits(exitRoom, null, null, baronRoom);
+//
+//        // 4. 아이템 및 몬스터 배치
+//        baronRoom.setMonster(new Monster("바론", 100, 20));
+//        baronRoom.setItem(new Item("바론 버프", "초고속 귀환"));
+//
+//        // 5. 시작 위치 설정
+//        currentRoom = startRoom;
+//    }
+//}
